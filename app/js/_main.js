89eb7ff543df113e67f6e15dca60f2ff2e86be01
Antonio Jesús <ajesus.etsii@gmail.com>
2014-11-23 23:15:56 +0100

    Added images and adapted to mobile

--- app/js/_main.js
+++ app/js/_main.js
@@ -9,54 +9,76 @@
  */
 
 ( function( $ ) {
-	
+
 	// Setup variables
 	$window = $(window);
 	$slide = $('.homeSlide');
-	$slideTall = $('.homeSlideTall');
-	$slideTall2 = $('.homeSlideTall2');
 	$body = $('body');
-	
-    //FadeIn all sections   
+
+    //FadeIn all sections
 	$body.imagesLoaded( function() {
 		setTimeout(function() {
-		      
+
 		      // Resize sections
 		      adjustWindow();
-		      
+
 		      // Fade in sections
 			  $body.removeClass('loading').addClass('loaded');
-			  
+
 		}, 800);
 	});
-	
+
 	function adjustWindow(){
-		
-		// Init Skrollr
-		var s = skrollr.init({
-		    render: function(data) {
-		    
-		        //Debugging - Log the current scroll position.
-		        //console.log(data.curTop);
-		    }
-		});
-		
+
 		// Get window size
-	    winH = $window.height();
-	    
-	    // Keep minimum height 550
-	    if(winH <= 550) {
+		winH = $window.height();
+		winW = $window.width();
+
+		// Keep minimum height 550
+		if(winH <= 550) {
 			winH = 550;
-		} 
-	    
-	    // Resize our slides
-	    $slide.height(winH);
-	    $slideTall.height(winH*2);
-	    $slideTall2.height(winH*3);
-	    
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
-	    
+		}
+
+		// Init Skrollr for 768 and up
+		if( winW >= 768) {
+
+			// Init Skrollr
+			var s = skrollr.init({
+				forceHeight: false
+			});
+
+			// Resize our slides
+			$slide.height(winH);
+
+			s.refresh($('.homeSlide'));
+
+		} else {
+
+			// Init Skrollr
+			var s = skrollr.init();
+			s.destroy();
+		}
+
+		// Check for touch
+		if(Modernizr.touch) {
+
+			// Init Skrollr
+			var s = skrollr.init();
+			s.destroy();
+		}
+
 	}
-		
+
+	//This function can be used by enquire.js to adjust window on resize
+	function initAdjustWindow() {
+	    return {
+	        match : function() {
+	            adjustWindow();
+	        },
+	        unmatch : function() {
+	            adjustWindow();
+	        }
+	    };
+	}
+
 } )( jQuery );
